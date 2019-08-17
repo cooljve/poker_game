136ce7a89fdde742da960407b7bec56a6e75271e
Joi Liu <547169735@qq.com>
2019-08-17 13:15:20 +0800

    complete type high card and refactor code

--- src/main/java/PokerGame.java
+++ src/main/java/PokerGame.java
@@ -9,12 +9,10 @@ public class PokerGame {
   public int judge(String card1, String card2) {
     List<Poker> pokerList1 = convertCards(card1);
     List<Poker> pokerList2 = convertCards(card2);
-    for (int i = pokerList1.size()-1; i >= 0; i--) {
+    for (int i = pokerList1.size() - 1; i >= 0; i--) {
       if (pokerList1.get(i).getSize() > pokerList2.get(i).getSize()) {
         return 1;
-      } else if (pokerList1.get(i).getSize() == pokerList2.get(i).getSize()) {
-        continue;
-      } else {
+      } else if (pokerList1.get(i).getSize() < pokerList2.get(i).getSize()) {
         return -1;
       }
     }
@@ -24,9 +22,9 @@ public class PokerGame {
   private List<Poker> convertCards(String cards) {
     List<String> cardList = new ArrayList<>(Arrays.asList(cards.split(" ")));
     List<Poker> pokerList = new ArrayList<>();
-    for (String card : cardList) {
-      pokerList.add(new Poker(card.substring(0, card.length() - 1), card.substring(card.length() - 1)));
-    }
+    cardList.stream()
+        .map(x -> new Poker(x.substring(0, x.length() - 1), x.substring(x.length() - 1)))
+        .forEach(pokerList::add);
     pokerList = pokerList.stream().sorted(Comparator.comparing(Poker::getSize)).collect(Collectors.toList());
     return pokerList;
   }
--- src/test/java/PokerGameTest.java
+++ src/test/java/PokerGameTest.java
@@ -1,9 +1,5 @@
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import static org.junit.Assert.assertEquals;
 
 public class PokerGameTest {
